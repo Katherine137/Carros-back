0a734024702788f08d3a978c2f0969c0f3ea9903
Katherine137 <katherine.sailema@epn.edu.ec>
2026-02-24 12:25:10 -0500

    actualizado

--- src/routers/reservas_routes.js
+++ src/routers/reservas_routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { actualizarReservas, crearReservas, eliminarReservas, listarReservas } from "../controllers/reservas_controller.js";
+import { actualizarReservas, crearReservas, eliminarReservas, listarReservas, obtenerReservasPorID } from "../controllers/reservas_controller.js";
 const router = Router()
 
 router.post('/Reservas', crearReservas)
 router.get('/listarRe', listarReservas)
+router.get('/obtenerRe', obtenerReservasPorID)
 router.put('/actualizarRe/:id', actualizarReservas)
 router.delete('/eliminarRe/:id', eliminarReservas)
 
